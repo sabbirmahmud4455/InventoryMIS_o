9831916e8ff3e03e7b6a63250c454dbf4bc38058
MahfuzNazib <nazibmahfuz60@gmail.com>
2022-06-22 11:30:14 +0600

    Basic Structure

---
+++ app/Models/SystemDataModule/ItemType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\SystemDataModule;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ItemType extends Model
+{
+    use HasFactory;
+}
