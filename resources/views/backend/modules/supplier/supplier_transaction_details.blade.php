5aa3593046c10f1eb0dc0e4da64337353d81f69c
sabbirmahmud4455 <sabbirmahmud4455@gmail.com>
2022-07-19 18:44:08 +0600

    supplier transaction issue fix

--- resources/views/backend/modules/supplier/supplier_transaction_details.blade.php
+++ resources/views/backend/modules/supplier/supplier_transaction_details.blade.php
@@ -48,7 +48,7 @@
 
             <tr>
                 <td>{{ __('Application.Remarks') }}</td>
-                <td colspan="3">{{ $transaction_details['transactions']->transaction_remarks ? $transaction_details->transaction_remarks : 'N/A' }}</td>
+                <td colspan="3">{{ $transaction_details['transactions']->transaction_remarks ? $transaction_details['transactions']->transaction_remarks : 'N/A' }}</td>
             </tr>
 
             {{-- Transaction Type Information --}}
@@ -77,43 +77,41 @@
         </tbody>
     </table>
 
-    @empty($transaction_details['purchase_details'])
-        {{-- Purchase Details --}}
-        <span class="badge badge-info">{{ __('Purchase.ViewPurchaseInfo') }}</span>
-        <table class="table table-sm table-bordered">
-            <thead>
-                <th>{{ __('Application.SerialNo') }}</th>
-                <th>{{ __('Lot.LotName') }}</th>
-                <th>{{ __('Item.Item') }}</th>
-                <th>{{ __('Purchase.Weight') }}</th>
-                <th>{{ __('Unit.Unit') }}</th>
-                <th>{{ __('Purchase.Price') }}</th>
-                <th>{{ __('Purchase.TotalPrice') }}</th>
-            </thead>
+    @if (!empty($transaction_details['purchase_details']))
+          {{-- Purchase Details --}}
+          <span class="badge badge-info">{{ __('Purchase.ViewPurchaseInfo') }}</span>
+          <table class="table table-sm table-bordered">
+              <thead>
+                  <th>{{ __('Application.SerialNo') }}</th>
+                  <th>{{ __('Lot.LotName') }}</th>
+                  <th>{{ __('Item.Item') }}</th>
+                  <th>{{ __('Purchase.Weight') }}</th>
+                  <th>{{ __('Unit.Unit') }}</th>
+                  <th>{{ __('Purchase.Price') }}</th>
+                  <th>{{ __('Purchase.TotalPrice') }}</th>
+              </thead>
 
-            <tbody>
-                @forelse ($transaction_details['purchase_details'] as $key => $purchase_details)
-                    <tr>
-                        <td>{{ $key + 1 }}</td>
-                        <td>{{ $purchase_details->lot_name }}</td>
-                        <td>{{ $purchase_details->item_name }}</td>
-                        <td>{{ $purchase_details->varient_name }}</td>
-                        <td>{{ $purchase_details->unit_name }}</td>
-                        <td>{{ $purchase_details->unit_price }}</td>
-                        <td>{{ $purchase_details->total_price }}</td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td>
-                            <span class="badge badge-danger">No Data Found!!</span>
-                        </td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
-    @else
-        <h5>sdfvdfs</h5>
-    @endempty
+              <tbody>
+                  @forelse ($transaction_details['purchase_details'] as $key => $purchase_details)
+                      <tr>
+                          <td>{{ $key + 1 }}</td>
+                          <td>{{ $purchase_details->lot_name }}</td>
+                          <td>{{ $purchase_details->item_name }}</td>
+                          <td>{{ $purchase_details->varient_name }}</td>
+                          <td>{{ $purchase_details->unit_name }}</td>
+                          <td>{{ $purchase_details->unit_price }}</td>
+                          <td>{{ $purchase_details->total_price }}</td>
+                      </tr>
+                  @empty
+                      <tr>
+                          <td>
+                              <span class="badge badge-danger">No Data Found!!</span>
+                          </td>
+                      </tr>
+                  @endforelse
+              </tbody>
+          </table>
+    @endif
 
 
 </div>
